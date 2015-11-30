c449e1f18315345a597f87ed024fb5982827c715
Jiang Yan <jiangy@dsp.ac.cn>
2015-11-30 17:13:15 +0800

    Update xwalk_settings.cc
    delete whitespaces

--- runtime/browser/android/xwalk_settings.cc
+++ runtime/browser/android/xwalk_settings.cc
@@ -78,8 +78,8 @@ struct XWalkSettings::FieldIds {
         GetFieldID(env, clazz, "mDefaultFontSize", "I");
     default_fixed_font_size =
         GetFieldID(env, clazz, "mDefaultFixedFontSize", "I");
-	spatial_navigation_enabled =
-	    GetFieldID(env, clazz, "mSpatialNavigationEnabled", "Z");
+    spatial_navigation_enabled =
+        GetFieldID(env, clazz, "mSpatialNavigationEnabled", "Z");
   }
 
   // Field ids
